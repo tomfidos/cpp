d39ac40ebaeccb7aa2c9d11f674f36d8b20cef1f
Tomasz <fidostomasz@gmail.com>
2020-08-29 17:25:23 +0200

    Adding a function getting the posters number needed to cover buildings.

--- plakatowanie.cpp
+++ plakatowanie.cpp
@@ -9,7 +9,7 @@ int getHeight() {
     std::getline(std::cin >> std::ws, dimensions);
     std::stringstream is(dimensions);
     int dimension;
-    int bothDimensions [2];
+    int bothDimensions [2]; 
     for (int i = 0; i < 2; i++) {
         is >> dimension;
         bothDimensions[i] = dimension;
@@ -17,19 +17,36 @@ int getHeight() {
     return bothDimensions[1];
 }
 
-std::vector<int> getBuildings() {
+std::vector<int> getBuildingHeights() {
     int buildingsNumber;
     std::cout << "Podaj liczbę budynków, a potem ich wymiary (długość wysokość): " << std::endl;
     std::ios_base::sync_with_stdio(false);
     std::cin >> buildingsNumber;
-    std::vector<int> buildings;
+    std::vector<int> buildingHeights;
     for (int i = 0; i < buildingsNumber; i++) {
-        int building = getHeight();
-        buildings.push_back(building);
+        int height = getHeight();
+        buildingHeights.push_back(height);
     }
-    return buildings;
+    return buildingHeights;
+}
+
+int getUtilizedPosters(std::vector<int> buildingHeights) {
+    std::vector<int> posters;
+    int utilizedPosters = 0;
+    for (std::vector<int>::iterator it = buildingHeights.begin(); it != buildingHeights.end(); ++it) {
+        while (posters.size() != 0 && posters.front() > *it) {
+            posters.erase(posters.begin());
+        }
+        if (posters.size() == 0 || posters.front() < *it) {
+            posters.insert(posters.begin(), *it);
+            utilizedPosters++;
+        }
+    }
+    return utilizedPosters;
 }
 
 int main() {
-    std::vector<int> buildings = getBuildings();
+    std::vector<int> buildingHeights = getBuildingHeights();
+    int utilizedPosters = getUtilizedPosters(buildingHeights);
+    std::cout << utilizedPosters << std::endl;
 }
