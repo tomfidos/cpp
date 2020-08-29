941fc4cf8ed77052c51bb80ce665d99473a3bf05
Tomasz <fidostomasz@gmail.com>
2020-08-29 15:42:02 +0200

    Adding a building height getting function.

--- plakatowanie.cpp
+++ plakatowanie.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 #include <cstdio>
 #include <vector>
+
+int getHeight() {
+    std::string dimensions;
+    std::getline(std::cin >> std::ws, dimensions);
+    std::stringstream is(dimensions);
+    int dimension;
+    int bothDimensions [2];
+    for (int i = 0; i < 2; i++) {
+        is >> dimension;
+        bothDimensions[i] = dimension;
+    }
+    return bothDimensions[1];
+}
 
 std::vector<int> getBuildings() {
     int buildingsNumber;
@@ -9,20 +24,12 @@ std::vector<int> getBuildings() {
     std::cin >> buildingsNumber;
     std::vector<int> buildings;
     for (int i = 0; i < buildingsNumber; i++) {
-        int building;
-        std::cin >> building;
+        int building = getHeight();
         buildings.push_back(building);
     }
     return buildings;
 }
 
-void printBuildings(std::vector<int> const &buildings ) {
-    for (int i = 0; i < buildings.size(); i++) {
-        std::cout << buildings.at(i) << std::endl;
-    }
-}
-
 int main() {
     std::vector<int> buildings = getBuildings();
-    printBuildings(buildings);
 }
